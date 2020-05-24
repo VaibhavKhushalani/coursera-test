d73b7b622c2ba01f0de60aef0eea00021a0a8eaf
VAibhavkhushalani <vaibhavkhushlani04@gmail.com>
2020-05-25 01:09:17 +0530

    my assingment

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+
+(function (window){
+var byeSpeaker = new Object();
+var speakWord = "Good Bye";
+byeSpeaker.speak = function speak(name){
+  console.log(speakWord + " " + name);
+};
+window.byeSpeaker = byeSpeaker;
+})(window);
+
